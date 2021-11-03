1c287ffb12051ff50127ce8bfaea81633ebe1531
dummysimple321 <dummy.simple321@gmail.com>
2021-11-03 18:45:54 +0530

    Changes to feature and stepdef

--- src/main/java/com/gmail/stepdefinition/ComposeMailSteps.java
+++ src/main/java/com/gmail/stepdefinition/ComposeMailSteps.java
@@ -1,19 +1,23 @@
 package com.gmail.stepdefinition;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class ComposeMailSteps {
+
     WebDriver driver;
 
     @Given("I open gmail page in browser")
@@ -23,7 +27,6 @@ public class ComposeMailSteps {
         driver.get("https://mail.google.com");
 
         driver.manage().window().maximize();
-//        driver.manage().deleteAllCookies();
 
         //Implicit Wait
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -66,5 +69,65 @@ public class ComposeMailSteps {
     @After
     public void tearDown(){
         driver.close();
+    }
+
+    @When("^I click on Compose button$")
+    public void iClickOnComposeButton() {
+        WebElement composeButton = driver.findElement(By.xpath("//div[@class = 'z0']"));
+        WebDriverWait waitForCompose = new WebDriverWait(driver,10);
+        waitForCompose.until(ExpectedConditions.visibilityOf(composeButton));
+        composeButton.click();
+    }
+
+    @And("^I enter the recipient email address \"([^\"]*)\"$")
+    public void iEnterTheRecipientEmailAddress(String receiptEmailAddr) throws Throwable {
+        WebElement toEmail =  driver.findElement(By.xpath("//textarea[@name='to']"));
+        waitForElementVisibility(toEmail);
+        toEmail.click();
+//        toEmail.sendKeys(Keys.BACK_SPACE);
+        toEmail.sendKeys(receiptEmailAddr);
+    }
+
+    private void waitForElementVisibility(WebElement element){
+        WebDriverWait waitForElemVisibility = new WebDriverWait(driver, 10);
+        waitForElemVisibility.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    private boolean isElementDisplayed(WebElement element){
+        WebDriverWait waitForElemVisibility = new WebDriverWait(driver, 10);
+        waitForElemVisibility.until(ExpectedConditions.textToBePresentInElement(element,"Message sent"));
+        return true;
+    }
+
+    @And("^i enter the subject \"([^\"]*)\"$")
+    public void iEnterTheSubject(String subTxt) throws Throwable {
+        WebElement subject = driver.findElement(By.xpath(("//input[@name='subjectbox']")));
+        waitForElementVisibility(subject);
+        subject.sendKeys(subTxt);
+        Thread.sleep(3000);
+    }
+
+    @And("^I enter the body \"([^\"]*)\"$")
+    public void iEnterTheBody(String bodyText) throws Throwable {
+        WebElement body = driver.findElement(By.xpath(("//table//td//div[@class='Am Al editable LW-avf tS-tW']")));
+        waitForElementVisibility(body);
+        body.sendKeys(bodyText);
+        Thread.sleep(3000);
+    }
+
+    @When("^I click on Send button$")
+    public void iClickOnSendButton() throws InterruptedException {
+        WebElement sendBtn = driver.findElement(By.xpath(("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")));
+        waitForElementVisibility(sendBtn);
+        sendBtn.click();
+        Thread.sleep(1000);
+    }
+
+    @Then("^mail should be sent successfully$")
+    public void mailShouldBeSentSuccessfully() throws InterruptedException {
+        WebElement message = driver. findElement(By.xpath(("//span[@class='aT']/span[text()='Message sent.']")));
+        waitForElementVisibility(message);
+        Assert.assertEquals("Confirmation message does not match",true, isElementDisplayed(message));
+        Thread.sleep(3000);
     }
 }
--- target/classes/features/compose_mail.feature
+++ target/classes/features/compose_mail.feature
@@ -1,8 +1,14 @@
 Feature: This is to test the compose mail of Gmail
 
-  Scenario: Compose email and send using gmail
+  Scenario: Compose email and send using Gmail
     Given I open gmail page in browser
     When I have provide mail id "dummy.simple321@gmail.com"
     And I provide the password "Simple#123"
-#    When I click on the Next button
+    When I click on the Next button
     Then user is able to see inbox
+    When I click on Compose button
+    And I enter the recipient email address "dummy.simple321@gmail.com"
+    And i enter the subject "Incubyte"
+    And I enter the body "Automation QA test for Incubyte"
+    When I click on Send button
+    Then mail should be sent successfully
